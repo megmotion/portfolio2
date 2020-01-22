8bdb2df4bff72e66e4c18dbe39d2b5325609e779
megmotion <33497517+megmotion@users.noreply.github.com>
2020-01-22 21:54:54 +0100

    add submit_form endpoint

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +9,11 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		print(data)
+		return redirect('/thankyou.html')
+	else: 
+		return 'something went wrong, try again!'
